b4d20dabeda6bf1b8866d95da5c26dd17a156d60
N1GHTMAR3 <70622133+n1ghtmar3-2421@users.noreply.github.com>
2021-03-02 19:13:28 +0600

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -6,7 +6,7 @@ resolvers="/root/resolvers.txt"
 domain_enum(){
 for domain in $(cat $host);
 do
-    mkdir -p $domain $domain/sources $domain/sources/dnsprobe $domain/Recon $domain/Recon/sub_takeover $domain/Recon/nuclei $domain/Recon/wayback $domain/Recon/gf $domain/Recon/wordlist $domain/Recon/masscan
+    mkdir -p $domain $domain/sources $domain/sources/dnsprobe $domain/sources/dns_query $domain/Recon $domain/Recon/sub_takeover $domain/Recon/nuclei $domain/Recon/wayback $domain/Recon/gf $domain/Recon/wordlist $domain/Recon/masscan
     subfinder -d $domain -o $domain/sources/subfinder.txt
     subdomain $domain | tee $domain/sources/subdomain.txt
     assetfinder -subs-only $domain | tee $domain/sources/domain.txt
@@ -17,6 +17,22 @@ do
 done
 }
 domain_enum
+dns_probe(){
+for domain in $(cat $host);
+do
+	dnsprobe -l $domain/sources/all.txt -r A -o $domain/sources/dns_query/A.txt
+	dnsprobe -l $domain/sources/all.txt -r NS -o $domain/sources/dns_query/NS.txt
+	dnsprobe -l $domain/sources/all.txt -r CNAME -o $domain/sources/dns_query/CNAME.txt
+	dnsprobe -l $domain/sources/all.txt -r SOA -o $domain/sources/dns_query/SOA.txt
+	dnsprobe -l $domain/sources/all.txt -r PTR -o $domain/sources/dns_query/PTR.txt
+	dnsprobe -l $domain/sources/all.txt -r MX -o $domain/sources/dns_query/MX.txt
+	dnsprobe -l $domain/sources/all.txt -r TXT -o $domain/sources/dns_query/TXT.txt
+	dnsprobe -l $domain/sources/all.txt -r AAAA -o $domain/sources/dns_query/AAAA.txt
+	dnsprobe -l $domain/sources/all.txt -f simple -o $domain/sources/dns_query/ip.txt
+	dnsprobe -l $domain/sources/all.txt -f full -o $domain/sources/dns_query/response.txt
+done
+}
+dns_probe
 sub_takeover(){
 for domain in $(cat $host);
 do
@@ -39,45 +55,6 @@ do
 	webtech --ul=$domain/Recon/httpx.txt | tee $domain/Recon/webtech.txt
 done
 }
-scanner(){
-for domain in $(cat $host);
-do
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/cves/ -c 50 -o $domain/Recon/nuclei/cves.txt
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/vulnerabilities/ -c 50 -o $domain/Recon/nuclei/vulnerabilities.txt
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/takeovers/ -o $domain/Recon/nuclei/sub_takeover.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/default-logins/ -o $domain/Recon/nuclei/default_credentials.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/exposed-tokens/ -o $domain/Recon/nuclei/tokens.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/dns/ -o $domain/Recon/nuclei/dns.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/misconfiguration/ -o $domain/Recon/nuclei/misconfiguration.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/exposed-panels/ -o $domain/Recon/nuclei/panels.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/technologies/ -o $domain/Recon/nuclei/technologies.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/exposures/ -o $domain/Recon/nuclei/exposures.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/helpers/ -o $domain/Recon/nuclei/helpers.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/fuzzing/arbitrary-file-read.yaml -o $domain/Recon/nuclei/arbitrary-file-read.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/fuzzing/directory-traversal.yaml -o $domain/Recon/nuclei/directory-traversal.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/fuzzing/generic-lfi-fuzzing.yaml -o $domain/Recon/nuclei/generic-lfi-fuzzing.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/fuzzing/iis-shortname.yaml -o $domain/Recon/nuclei/iis-shortname.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/artica-web-proxy-workflow.yaml -o $domain/Recon/nuclei/artica-web-proxy-workflow.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/bigip-workflow.yaml -o $domain/Recon/nuclei/bigip-workflow.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/cisco-asa-workflow.yaml -o $domain/Recon/nuclei/cisco-asa-workflow.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/grafana-workflow.yaml -o $domain/Recon/nuclei/grafana-workflow.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/jira-workflow.yaml -o $domain/Recon/nuclei/jira-workflow.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/liferay-workflow.yaml -o $domain/Recon/nuclei/liferay-workflow.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/lotus-domino-workflow.yaml -o $domain/Recon/nuclei/lotus-domino-workflow.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/magmi-workflow.yaml -o $domain/Recon/nuclei/magmi-workflow.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/mida-eframework-workflow.yaml -o $domain/Recon/nuclei/mida-eframework-workflow.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/netsweeper-workflow.yaml -o $domain/Recon/nuclei/netsweeper-workflow.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/rabbitmq-workflow.yaml -o $domain/Recon/nuclei/rabbitmq-workflow.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/sap-netweaver-workflow.yaml -o $domain/Recon/nuclei/sap-netweaver-workflow.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/solarwinds-orion-workflow.yaml -o $domain/Recon/nuclei/solarwinds-orion-workflow.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/springboot-workflow.yaml -o $domain/Recon/nuclei/springboot-workflow.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/thinkphp-workflow.yaml -o $domain/Recon/nuclei/thinkphp-workflow.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/vbulletin-workflow.yaml -o $domain/Recon/nuclei/vbulletin-workflow.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/weblogic-workflow.yaml -o $domain/Recon/nuclei/weblogic-workflow.yaml.txt -v
-	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/wordpress-workflow.yaml -o $domain/Recon/nuclei/wordpress-workflow.yaml.txt -v
-done
-}
-scanner
 wayback_data(){
 for domain in $(cat $host);
 do
@@ -120,3 +97,42 @@ do
 done
 }
 get_ip
+scanner(){
+for domain in $(cat $host);
+do
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/cves/ -c 50 -o $domain/Recon/nuclei/cves.txt
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/vulnerabilities/ -c 50 -o $domain/Recon/nuclei/vulnerabilities.txt
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/takeovers/ -o $domain/Recon/nuclei/sub_takeover.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/default-logins/ -o $domain/Recon/nuclei/default_credentials.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/exposed-tokens/ -o $domain/Recon/nuclei/tokens.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/dns/ -o $domain/Recon/nuclei/dns.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/misconfiguration/ -o $domain/Recon/nuclei/misconfiguration.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/exposed-panels/ -o $domain/Recon/nuclei/panels.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/technologies/ -o $domain/Recon/nuclei/technologies.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/exposures/ -o $domain/Recon/nuclei/exposures.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/helpers/ -o $domain/Recon/nuclei/helpers.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/fuzzing/arbitrary-file-read.yaml -o $domain/Recon/nuclei/arbitrary-file-read.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/fuzzing/directory-traversal.yaml -o $domain/Recon/nuclei/directory-traversal.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/fuzzing/generic-lfi-fuzzing.yaml -o $domain/Recon/nuclei/generic-lfi-fuzzing.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/fuzzing/iis-shortname.yaml -o $domain/Recon/nuclei/iis-shortname.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/artica-web-proxy-workflow.yaml -o $domain/Recon/nuclei/artica-web-proxy-workflow.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/bigip-workflow.yaml -o $domain/Recon/nuclei/bigip-workflow.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/cisco-asa-workflow.yaml -o $domain/Recon/nuclei/cisco-asa-workflow.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/grafana-workflow.yaml -o $domain/Recon/nuclei/grafana-workflow.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/jira-workflow.yaml -o $domain/Recon/nuclei/jira-workflow.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/liferay-workflow.yaml -o $domain/Recon/nuclei/liferay-workflow.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/lotus-domino-workflow.yaml -o $domain/Recon/nuclei/lotus-domino-workflow.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/magmi-workflow.yaml -o $domain/Recon/nuclei/magmi-workflow.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/mida-eframework-workflow.yaml -o $domain/Recon/nuclei/mida-eframework-workflow.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/netsweeper-workflow.yaml -o $domain/Recon/nuclei/netsweeper-workflow.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/rabbitmq-workflow.yaml -o $domain/Recon/nuclei/rabbitmq-workflow.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/sap-netweaver-workflow.yaml -o $domain/Recon/nuclei/sap-netweaver-workflow.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/solarwinds-orion-workflow.yaml -o $domain/Recon/nuclei/solarwinds-orion-workflow.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/springboot-workflow.yaml -o $domain/Recon/nuclei/springboot-workflow.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/thinkphp-workflow.yaml -o $domain/Recon/nuclei/thinkphp-workflow.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/vbulletin-workflow.yaml -o $domain/Recon/nuclei/vbulletin-workflow.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/weblogic-workflow.yaml -o $domain/Recon/nuclei/weblogic-workflow.yaml.txt -v
+	cat $domain/Recon/httpx.txt | nuclei -t /root/nuclei-templates/workflows/wordpress-workflow.yaml -o $domain/Recon/nuclei/wordpress-workflow.yaml.txt -v
+done
+}
+scanner
